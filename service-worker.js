ab29e0a4154a080b7352492acd589b6fea997144
haphut <haphut@users.noreply.github.com>
2020-04-05 11:30:48 +0000

    deploy: 3f897ac0901a49087f23f2fba1010266097dd253

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/gatesolve-ui/precache-manifest.44b2af61e59476df9165b05ce38126da.js"
+  "/gatesolve-ui/precache-manifest.1548c225ffb9c73d0bba2a222c59765a.js"
 );
 
 self.addEventListener('message', (event) => {
