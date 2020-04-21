47ce891f1d982a53b8bfb7816f5c474fc58fc1b6
Tuukka Hastrup <tuukka@sproutverse.com>
2020-04-21 14:54:12 +0300

    Show every entrance on the map

--- src/map-style.ts
+++ src/map-style.ts
@@ -22,6 +22,52 @@ export const routePointLayer = {
   filter: ["==", "Point", ["geometry-type"]],
 };
 
+export const allEntrancesLayer = {
+  id: "entrance-point",
+  "source-layer": "osm",
+  type: "circle",
+  minzoom: 12,
+  paint: {
+    "circle-radius": [
+      "interpolate",
+      ["linear"],
+      ["zoom"],
+      12, // At zoom 12 or less,
+      1, // circle radius is 1.
+      14, // At zoom 14,
+      3, // circle radius is 3.
+      15, // At zoom 15 or more,
+      5, // circle radius is 5.
+    ] as Expression,
+    "circle-color": "#64be14",
+  },
+  filter: ["has", "entrance"],
+};
+
+export const allEntrancesSymbolLayer = {
+  id: "entrance-symbol",
+  "source-layer": "osm",
+  type: "symbol",
+  minzoom: 15,
+  paint: {
+    "text-color": "#000",
+    "text-halo-color": "#fff",
+    "text-halo-width": 3,
+  },
+  layout: {
+    "text-field": [
+      "coalesce",
+      ["get", "ref"],
+      ["get", "addr:unit"],
+    ] as Expression,
+    "text-anchor": ("center" as unknown) as Expression,
+    "text-font": ["Klokantech Noto Sans Regular"],
+    "text-size": 24,
+    "text-offset": [0, -0.05],
+  },
+  filter: ["has", "entrance"],
+};
+
 export const routePointSymbolLayer = {
   id: "route-point-symbol",
   type: "symbol",
