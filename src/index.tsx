99479d07d1fc7f0bfee0945f231f06fb7b0765ed
haphut <haphut@mistmap.com>
2020-06-02 04:16:57 +0300

    Use CircularProgress before App is loaded

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React, { Suspense } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import { SnackbarProvider } from "notistack";
+import { CircularProgress } from "@material-ui/core";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 
@@ -17,7 +18,22 @@ ReactDOM.render(
   <React.StrictMode>
     <Router>
       <SnackbarProvider dense maxSnack={3}>
-        <Suspense fallback={<div>loading</div>}>
+        <Suspense
+          fallback={
+            <div
+              style={{
+                width: "100%",
+                height: "100%",
+                position: "fixed",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+            >
+              <CircularProgress />
+            </div>
+          }
+        >
           <App />
         </Suspense>
       </SnackbarProvider>
