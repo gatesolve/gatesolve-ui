3d6148a7a2517633aef5736822c8bca2b8571bae
haphut <haphut@mistmap.com>
2020-12-09 16:04:26 +0200

    Show only one OLMap photo

--- src/components/OLMapImages.tsx
+++ src/components/OLMapImages.tsx
@@ -2,24 +2,16 @@ import React from "react";
 
 import { olmapNoteURL, NetworkState, OlmapResponse } from "../olmap";
 
-import "./OLMapImages.css";
-
 interface OLMapImagesProps {
   olmapData?: NetworkState<OlmapResponse>;
 }
 
 const OLMapImages: React.FC<OLMapImagesProps> = ({ olmapData }) => {
-  let content;
-  if (!olmapData) {
-    content = "";
-  } else if (olmapData.state === "loading") {
-    content = <div className="loading" />;
-  } else if (
-    olmapData.state === "success" &&
-    olmapData.response.image_notes?.filter((note) => note.image).length
-  ) {
-    content = olmapData.response.image_notes
-      .filter((note) => note.image)
+  const content =
+    olmapData?.state === "success" &&
+    olmapData?.response?.image_notes
+      ?.filter((note) => note.image)
+      .slice(0, 1) // take 1
       .map((note) => (
         <a
           key={note.id}
@@ -28,12 +20,13 @@ const OLMapImages: React.FC<OLMapImagesProps> = ({ olmapData }) => {
           rel="noopener noreferrer"
         >
           {/* eslint-disable-next-line jsx-a11y/img-redundant-alt */}
-          <img src={note.image} alt="A photo of the entrance" />
+          <img
+            style={{ maxWidth: "250px", maxHeight: "150px" }}
+            src={note.image}
+            alt="A photo of the entrance"
+          />
         </a>
       ));
-  } else {
-    content = <div className="empty" />;
-  }
   return <div className="olmapImages">{content}</div>;
 };
 
